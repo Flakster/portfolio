026e7b3d1d586f29c90474570c38b1f7f4dcbdf7
flakster <carlos.santamaria@gmail.com>
2020-09-13 11:31:54 -0500

    Refactor the whole page

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function closePreview(window){
+  document.getElementsByClassName(window)[0].style.display="none";
+}
+
+function showPreview(window){
+  document.getElementsByClassName(window)[0].style.display="block";
+}
